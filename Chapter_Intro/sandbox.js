e6ca5e349512d1181d21b41dca7d46d1ba49a980
Pierce Woods <pierce.woods95@gmail.com>
2019-07-24 19:23:44 +0100

    Returning values

--- Chapter_Intro/sandbox.js
+++ Chapter_Intro/sandbox.js
@@ -1,9 +1,19 @@
-let userName = 'Joddde'; // input value from user
-let currentTime = 'night'; //get current time from computer clock
+const speak = function(name = 'luigi', time = 'night') {
+    console.log(`I hate ${name} when it is ${time} time.`);
+};
 
-const speak = function(name = userName, time = currentTime){
-    console.log(`Hello ${name}, it is ${time} time.`);
+const calcArea = function(radius) {
+    return 3.14 * radius**2;
 }
 
-// input your details
-speak('dante');
+const area = calcArea(10);
+
+if(area > 200) {
+    console.log(`Wow, that is a fat area! ${area} is the biggest I have ever seen.`);
+} else {
+    console.log(`Pfft only ${area}. My willy is bigger than that.`)
+}
+
+// console.log(area);
+
+speak();
