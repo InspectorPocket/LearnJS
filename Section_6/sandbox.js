bf71f8bc9e10ee7d5b6fc9a07e33896d10b4cd1a
Pierce <pierce.woods95@gmail.com>
2019-08-27 14:20:47 +0100

    The Query Selector

---
+++ Section_6/sandbox.js
@@ -0,0 +1,14 @@
+// goes from top to bottom and grabs the first p tag
+// const para = document.querySelector('div.error');
+
+// console.log(para);
+
+// paras.forEach(para => {
+//     console.log(para);
+// });
+
+const paras = document.querySelectorAll('p');
+const error = document.querySelectorAll('.error');
+
+
+console.log(paras[0], error);
