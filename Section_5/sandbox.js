b8eb3f0f2482f4c7b3f83dd443b8915d6123d891
Pierce <pierce.woods95@gmail.com>
2019-08-27 11:39:08 +0100

    'this' Keyword

--- Section_5/sandbox.js
+++ Section_5/sandbox.js
@@ -1,5 +1,4 @@
 // object literals
-const name = 'pee-pee';
 
 let user = {
     name: 'phil mitchell',
@@ -7,23 +6,22 @@ let user = {
     email: 'phil.mitchel@ardbloke.com',
     location: 'strangeways',
     blogs: ['Breaking out of prison 101', 'I will bang out your dad'],
+    // below can be shortened to login() see below examples.
     login: function() {
         console.log(name.toUpperCase(),'logged in');
     },
-    logout: function() {
+    logout() {
         console.log('the user logged out');
     },
-    logBlogs: function() {
-
+    // in order to use this in the context of a method, you cannot use arrow functions.
+    // an arrow function will make the 'this' refer to the window and not the object
+    logBlogs() {
+        // console.log(this.blogs[0]);
+        console.log('the user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        });
     }
 };
 
-let status = true;
-
-if (status) {
-    user.login();
-} else {
-    user.logout();
-}
-
-name.toUpperCase();
+user.logBlogs();
