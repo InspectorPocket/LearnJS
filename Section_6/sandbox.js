07a3a9e5c0750634222bb4cd4386489e3a45c30e
Pierce <pierce.woods95@gmail.com>
2019-08-29 10:10:07 +0100

    Event Bubbling (and delegation)

--- Section_6/sandbox.js
+++ Section_6/sandbox.js
@@ -1,17 +1,59 @@
-const article = document.querySelector('article');
+const button = document.querySelector('button');
+const items = document.querySelectorAll('li');
+const ul = document.querySelector('ul');
+let newItem = 'Kill Sheldon Cooper';
 
-console.log(article.children);
-console.log(Array.from(article.children));
+button.addEventListener('click', () => {
+    button.innerText = 'BAZINGA';
+    // ul.innerHTML += `<li>${newItem}</li>`;
 
-Array.from(article.children).forEach(child => {
-    child.classList.add('article-element');
+    // creates li in JS, but it only exists in the void atm
+    const li = document.createElement('li');
+    li.textContent = 'KILL Sheldon Cooper';
+    // this will apend(at teh bottom) or prepend (at the top) the li to ul
+    ul.append(li);
+    // ul.prepend(li);
 });
 
-const title = document.querySelector('h2');
+// my go
+// items.forEach(item => {
+//     item.addEventListener('click', () => {
+//         item.setAttribute('style', 'display:none;');
+//     });
+// });
 
-// parentElement.parentElement can be stacked to look up the node tree
-console.log(title.parentElement.parentElement);
-console.log(title.nextElementSibling.previousElementSibling);
+// net ninjas go
+// e is a variable. it represents the event. In this case, an event listener.
+// items.forEach(item => {
+//     item.addEventListener('click', e => {
+//         you can console.log(e); to find out what events are available (eg)
+//         console.log(e.target);
+        
+//         e.target.style.textDecoration = 'line-through';
+//         e.target.style.color = 'grey';
 
-// chaining
-console.log(Array.from(title.nextElementSibling.parentElement.children));
+//         e.target.remove();
+//     });
+// });
+
+// items.forEach(item => {
+//     item becomes an event target
+//     item.addEventListener('click', e => {
+//         console.log('event in LI');
+//         this stops the event from bubbling up to the parent (ul), so only this event will fire
+//         e.stopPropagation();
+//         e.target.remove();
+//     });
+// });
+
+
+
+// shortened
+// e = event parameter
+// when this callback function fires, we know it is coming from the ul
+ul.addEventListener('click', e => {
+    // console.log('event in UL');
+    if (e.target.tagName === 'LI') {
+        e.target.remove();
+    }
+});
