13e7bb3a94e883ac687bab2715a688c377d7c861
Pierce <pierce.woods95@gmail.com>
2020-01-02 15:55:40 +0000

    Section_09 - Find Method

--- Section_09/sandbox.js
+++ Section_09/sandbox.js
@@ -1,27 +1,6 @@
-// reduce method
-// const scores = [10, 20, 60, 40, 70, 90, 30];
+// fine method
+const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
-// const result = scores.reduce ((acc, curr) => {
-//     if (curr > 50) {
-//         acc++;
-//     }
-//     return acc;
-// }, 0);
+const firstHighScore = scores.find(score => score > 50);
 
-// console.log(result);
-
-const scores = [
-    {player: 'mario', score: 50},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 70},
-    {player: 'peach', score: 60}
-];
-
-const marioTotal = scores.reduce((acc, curr) => {
-    if(curr.player === 'mario') {
-        acc += curr.score;
-    }
-    return acc;
-}, 0);
-
-console.log(marioTotal);
+console.log(firstHighScore);
