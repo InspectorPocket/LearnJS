5ff981a813e6ae6417d8db5539aee5bf1208c8d9
Pierce <pierce.woods95@gmail.com>
2020-01-02 17:13:24 +0000

    Section_09 - Sort Method

--- Section_09/sandbox.js
+++ Section_09/sandbox.js
@@ -1,6 +1,42 @@
-// fine method
-const scores = [10, 5, 0, 40, 60, 10, 20, 70];
+// SORT METHODS
+// ANCHOR Example #1 - Sorting Strings
+const names = ['ringo', 'pingo', 'flingo', 'gringo', 'dingo'];
 
-const firstHighScore = scores.find(score => score > 50);
+names.sort();
+names.reverse();
 
-console.log(firstHighScore);
+console.log('1. ', names);
+
+// ANCHOR Example #2 - Sorting Numbers
+const scores = [10, 50, 20, 5, 35, 70, 45];
+
+// if a number is a positive, it is greater than, and therefore moves up in rank
+scores.sort((a,b) => a - b);
+
+scores.reverse();
+
+console.log('2. ', scores);
+
+// ANCHOR Example #3 - Sorting Objects
+const players = [
+    {name: 'ringo', score: 20},
+    {name: 'pingo', score: 10},
+    {name: 'flingo', score: 50},
+    {name: 'gringo', score: 30},
+    {name: 'dingo', score: 70}
+];
+
+// players.sort((a,b) => {
+//     if (a.score > b.score) {
+//         return 1;
+//     } else if (a.score < b.score) {
+//         return -1;
+//     } else {
+//         return 0;
+//     }
+// });
+
+// 
+players.sort((a,b) => a.score - b.score);
+
+console.log('3. ', players);
