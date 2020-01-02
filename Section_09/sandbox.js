3fe43ba379b1bbe2fb200510588275b21b98eaf1
Pierce <pierce.woods95@gmail.com>
2020-01-02 15:51:33 +0000

    Section_09 - Reduce Method

--- Section_09/sandbox.js
+++ Section_09/sandbox.js
@@ -1,63 +1,27 @@
-// ANCHOR Regular Array Example
+// reduce method
+// const scores = [10, 20, 60, 40, 70, 90, 30];
 
-// const scores = [10, 30, 15, 25, 50, 40, 5];
-// const filteredScores = scores.filter(score => {
-//     return score > 20;
-// });
-
-// console.log(filteredScores);
-
-// const users = [
-//     {name: 'dingo', premium: true},
-//     {name: 'ringo', premium: false},
-//     {name: 'bingo', premium: false},
-//     {name: 'pingo', premium: true}
-// ];
-
-// const premiumUsers = users.filter(user => user.premium);
-// console.log(premiumUsers);
-
-// ANCHOR Map Array
-const prices = [ 20, 10, 30, 25, 15, 40, 80, 5];
-
-// Map Intro
-// const salePrices = prices.map(price => price / 2);
-// console.log(salePrices);
-
-// ANCHOR My Attempt
-// const blackMarket = [
-//     {name: 'dingo', price: 20},
-//     {name: 'ringo', price: 40},
-//     {name: 'bingo', price: 30},
-//     {name: 'pingo', price: 10},
-//     {name: 'gringo', price: 50}
-// ];
-
-// const saleProducts = blackMarket.map(product => {
-//     // half price when price is above 30
-//     if (product.price > 30) {
-//         return product.price / 2;
-//     } else {
-//         return product.price;
+// const result = scores.reduce ((acc, curr) => {
+//     if (curr > 50) {
+//         acc++;
 //     }
-// });
-// console.log(saleProducts);
+//     return acc;
+// }, 0);
 
-// ANCHOR Answer
-const blackMarket = [
-    {name: 'dingo', price: 20},
-    {name: 'ringo', price: 40},
-    {name: 'bingo', price: 30},
-    {name: 'pingo', price: 10},
-    {name: 'gringo', price: 50}
+// console.log(result);
+
+const scores = [
+    {player: 'mario', score: 50},
+    {player: 'yoshi', score: 30},
+    {player: 'mario', score: 70},
+    {player: 'peach', score: 60}
 ];
 
-const saleProducts = blackMarket.map(product => {
-    // half price when price is above 30
-    if (product.price > 30) {
-        return {name: product.name, price: product.price / 2};
-    } else {
-        return product;
+const marioTotal = scores.reduce((acc, curr) => {
+    if(curr.player === 'mario') {
+        acc += curr.score;
     }
-});
-console.log(saleProducts);
+    return acc;
+}, 0);
+
+console.log(marioTotal);
