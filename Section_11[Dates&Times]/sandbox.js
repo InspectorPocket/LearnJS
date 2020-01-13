84e900ef9344d659def6010930775678072df1ef
Pierce <pierce.woods95@gmail.com>
2020-01-13 15:56:36 +0000

    Section_12 - HTTP Requests(XHR) Pt2
    Reverted accidental changes to Section 11.

--- Section_11[Dates&Times]/sandbox.js
+++ Section_11[Dates&Times]/sandbox.js
@@ -1,9 +1,42 @@
-console.log(1);
-console.log(2);
+const clock = document.querySelector('.clock');
 
-setTimeout(() => {
-    console.log('callback function fired');
-}, 2000);
 
-console.log(3);
-console.log(4);
+const tick = () => {
+
+    const now = new Date();
+
+    const h = now.getHours();
+    const m = now.getMinutes();
+    const s = now.getSeconds();
+
+    const html = `
+        <span>${h}</span> : 
+        <span>${m}</span> : 
+        <span>${s}</span>
+    `;
+    
+    clock.innerHTML = html;
+
+};
+
+setInterval(tick,1000);
+
+
+
+
+
+const now = new Date();
+
+// console.log(dateFns.isToday(now));
+
+// formatting options
+console.log(dateFns.format(now, 'YYYY'));
+console.log(dateFns.format(now, 'MMM'));
+console.log(dateFns.format(now, 'dddd'));
+console.log(dateFns.format(now, 'Do'));
+console.log(dateFns.format(now, 'dddd Do MMMM YYYY'));
+
+// comparing dates
+const before = new Date('Feburary 1 2019 12:00:00');
+
+console.log(dateFns.distanceInWords(now, before, {addSuffix: true}));
