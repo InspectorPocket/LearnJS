6dbb234e29b63360c0b8f4b29cc58da8e69be633
Pierce <pierce.woods95@gmail.com>
2020-01-13 15:21:11 +0000

    Section_11 - Date Fns Library

--- Section_11[Dates&Times]/sandbox.js
+++ Section_11[Dates&Times]/sandbox.js
@@ -2,7 +2,7 @@ const clock = document.querySelector('.clock');
 
 
 const tick = () => {
-    
+
     const now = new Date();
 
     const h = now.getHours();
@@ -19,5 +19,24 @@ const tick = () => {
 
 };
 
-
 setInterval(tick,1000);
+
+
+
+
+
+const now = new Date();
+
+// console.log(dateFns.isToday(now));
+
+// formatting options
+console.log(dateFns.format(now, 'YYYY'));
+console.log(dateFns.format(now, 'MMM'));
+console.log(dateFns.format(now, 'dddd'));
+console.log(dateFns.format(now, 'Do'));
+console.log(dateFns.format(now, 'dddd Do MMMM YYYY'));
+
+// comparing dates
+const before = new Date('Feburary 1 2019 12:00:00');
+
+console.log(dateFns.distanceInWords(now, before, {addSuffix: true}));
