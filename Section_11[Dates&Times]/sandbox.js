4ec8aab96d1e0fcad4ab94e16af3c254358ec327
Pierce <pierce.woods95@gmail.com>
2020-01-13 15:36:37 +0000

    Section_12 - Initial Setup + Intro

--- Section_11[Dates&Times]/sandbox.js
+++ Section_11[Dates&Times]/sandbox.js
@@ -1,42 +1,9 @@
-const clock = document.querySelector('.clock');
+console.log(1);
+console.log(2);
 
+setTimeout(() => {
+    console.log('callback function fired');
+}, 2000);
 
-const tick = () => {
-
-    const now = new Date();
-
-    const h = now.getHours();
-    const m = now.getMinutes();
-    const s = now.getSeconds();
-
-    const html = `
-        <span>${h}</span> : 
-        <span>${m}</span> : 
-        <span>${s}</span>
-    `;
-    
-    clock.innerHTML = html;
-
-};
-
-setInterval(tick,1000);
-
-
-
-
-
-const now = new Date();
-
-// console.log(dateFns.isToday(now));
-
-// formatting options
-console.log(dateFns.format(now, 'YYYY'));
-console.log(dateFns.format(now, 'MMM'));
-console.log(dateFns.format(now, 'dddd'));
-console.log(dateFns.format(now, 'Do'));
-console.log(dateFns.format(now, 'dddd Do MMMM YYYY'));
-
-// comparing dates
-const before = new Date('Feburary 1 2019 12:00:00');
-
-console.log(dateFns.distanceInWords(now, before, {addSuffix: true}));
+console.log(3);
+console.log(4);
