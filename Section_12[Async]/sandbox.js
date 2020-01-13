a5c5f1a3a904d7e25d8cbc38194f0391493ee1e1
Pierce <pierce.woods95@gmail.com>
2020-01-13 17:14:49 +0000

    Section_12 - Callback Functions

--- Section_12[Async]/sandbox.js
+++ Section_12[Async]/sandbox.js
@@ -1,12 +1,30 @@
-const request = new XMLHttpRequest();
+const getTodos = callback => {
+    const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
-    if (request.readyState === 4 && request.status === 200) {
-        console.log(request, request.responseText);
-    } else if (request.readyState === 4) {
-        console.log('ERROR. FETCH DATA. NO NO.');
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            callback(undefined, request.responseText);
+        } else if (request.readyState === 4) {
+            callback('could not fetch data', undefined);
+        }
+    });
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+};
+
+console.log(1);
+console.log(2);
+
+//  this will chase for the inforamtion whilst the other functions are being applied
+getTodos((err, data) => {
+    console.log('callback fired');
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
     }
 });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+console.log(3);
+console.log(4);
