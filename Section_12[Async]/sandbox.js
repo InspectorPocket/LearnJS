f2855d41010f8cf0dd33be8e6a9f60b35d071915
Pierce <pierce.woods95@gmail.com>
2020-01-20 16:36:30 +0000

    [section_12] The Fetch API

--- Section_12[Async]/sandbox.js
+++ Section_12[Async]/sandbox.js
@@ -1,31 +1,14 @@
-const getTodos = resource => {
-    const request = new XMLHttpRequest();
+// Fetch API
+// (1) fetch data url
+// (2) take response and return as json
+// (3) .then data, log or do with data what you will
+// (4) add .catch to spit out any errors
 
-    return new Promise((resolve, reject) => {
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                resolve(data);
-            } else if (request.readyState === 4) {
-                reject('could not load resource');
-            }
-        });
-    
-        request.open('GET', resource);
-        request.send();
-    });
-};
-
-// once luigi is called, display resolve
-// then return the next getTodos
-getTodos('todos/luigi.json').then(data => {
-    console.log('promise 1 resolved:', data);
-    return getTodos('todos/mario.json');
+fetch('todos/luigi.json').then(response => {
+    console.log('resolved:', response);
+    return response.json();
 }).then(data => {
-    console.log('promise 2 resolved:', data);
-    return getTodos('todos/pierce.json');
-}).then(data => {
-    console.log('promise 3 resolved:', data);
+    console.log(data);
 }).catch(err => {
-    console.log('promise rejected:', err);
+    console.log('rejected:', err);
 });
