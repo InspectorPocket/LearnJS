46c612edc7cafff6701f578cc82133dbcbfb2732
Pierce <pierce.woods95@gmail.com>
2020-01-20 10:42:32 +0000

    [section_12] JSON Data

--- Section_12[Async]/sandbox.js
+++ Section_12[Async]/sandbox.js
@@ -3,20 +3,24 @@ const getTodos = callback => {
 
     request.addEventListener('readystatechange', () => {
         if (request.readyState === 4 && request.status === 200) {
-            callback(undefined, request.responseText);
+            // parses JSON strings into JS objects
+            const data = JSON.parse(request.responseText);
+            // triggers the getTodos function
+            callback(undefined, data);
         } else if (request.readyState === 4) {
             callback('could not fetch data', undefined);
         }
     });
 
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.open('GET', 'todos.json');
     request.send();
 };
 
 console.log(1);
 console.log(2);
 
-//  this will chase for the inforamtion whilst the other functions are being applied
+//  this will chase for the information whilst the other functions are being applied
+// always error first
 getTodos((err, data) => {
     console.log('callback fired');
     if (err) {
