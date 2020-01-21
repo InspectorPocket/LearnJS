e2431ef497d42d7144715894460f5f9dd03c4f66
Pierce <pierce.woods95@gmail.com>
2020-01-21 09:42:07 +0000

    [section_12] Async & Await

--- Section_12[Async]/sandbox.js
+++ Section_12[Async]/sandbox.js
@@ -1,14 +1,29 @@
-// Fetch API
-// (1) fetch data url
-// (2) take response and return as json
-// (3) .then data, log or do with data what you will
-// (4) add .catch to spit out any errors
+// Async & Await
+    // async always returns a promise
 
-fetch('todos/luigi.json').then(response => {
-    console.log('resolved:', response);
-    return response.json();
-}).then(data => {
-    console.log(data);
-}).catch(err => {
-    console.log('rejected:', err);
-});
+const getTodos = async () => {
+
+    const response = await fetch('todos/luigi.json');
+    const data = response.json();
+    
+    return data;
+};
+
+getTodos()
+    .then(data => console.log('resolved:', data));
+
+
+// Fetch API
+    // (1) fetch data url
+    // (2) take response and return as json
+    // (3) .then data, log or do with data what you will
+    // (4) add .catch to spit out any errors
+
+// fetch('todos/luigi.json').then(response => {
+//     console.log('resolved:', response);
+//     return response.json();
+// }).then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log('rejected:', err);
+// });
