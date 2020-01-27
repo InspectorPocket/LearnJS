0f1e9763b1f6fbe84eb6f533bd1deddaeb1fe392
Pierce <pierce.woods95@gmail.com>
2020-01-27 16:45:39 +0000

    [section_13] Get Weather API Call

--- Section_13[WeatherApp]/scripts/forecast.js
+++ Section_13[WeatherApp]/scripts/forecast.js
@@ -1,15 +1,27 @@
 const key = 'AIkjgA3FlkGlwvGQjLIWqHasZ6ACXRKr';
 let city = 'Preston';
 
+// Get weather information
+const getWeather = async id => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    
+    return data[0];
+
+}
+
+// Get city information
 const getCity = async city => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     // Manoj solution
     // const response = await fetch(base + query).then(response => response.json());
-
     // return response[0];
-
+    
     const response = await fetch(base + query);
     const data = await response.json();
 
@@ -17,5 +29,8 @@ const getCity = async city => {
 };
 
 getCity(city)
-    .then(data => console.log(data))
+    .then(data => { return getWeather(data.Key); })
+    .then(data => {
+        console.log(data);
+    })
     .catch(err => console.log(err.message));
