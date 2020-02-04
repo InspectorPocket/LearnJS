b4f206fe97e0a4dd2d5477498663bbd783c87c29
Pierce <pierce.woods95@gmail.com>
2020-02-04 15:31:31 +0000

    [section_14] Section Complete

--- Section_13&14[WeatherApp]/scripts/app.js
+++ Section_13&14[WeatherApp]/scripts/app.js
@@ -52,4 +52,15 @@ cityForm.addEventListener('submit', e => {
     updateCity(city)
         .then(data => updateUI(data))
         .catch(err => console.log(err));
+
+        // set local storage
+        localStorage.setItem('city', city);
+
 });
+
+if(localStorage.getItem('city')) {
+    // Update UI with data
+    updateCity(localStorage.getItem('city'))
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+}
