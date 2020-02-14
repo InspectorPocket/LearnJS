2ba820e3254048487de23eb4ee44704304fc491e
Pierce <pierce.woods95@gmail.com>
2020-02-14 11:31:11 +0000

    [section_15] Class Constructors

--- Section_15[OOP]/sandbox.js
+++ Section_15[OOP]/sandbox.js
@@ -1,15 +1,18 @@
-const userOne = {
-    username: 'ryu',
-    email: 'ryu@gmail.com',
-    login() {
-        console.log('user has logged in');
-    },
-    logout() {
-        console.log('user has logged out');
+class User {
+    constructor(username, email) {
+        // set up properties
+        this.username = username;
+        this.email = email;
     }
 };
 
-console.log(userOne.email, userOne.username);
-userOne.login();
+const userOne = new User('mario', 'mario@nintendo.com');
+const userTwo = new User('luigi', 'luigi@nintendo.com');
 
-const userTwo = new User('pierce@gmail.com', 'pierce');
+console.log(userOne, userTwo);
+
+
+// the 'new' keyword
+// 1. creates empty object 
+// 2. binds value of 'this' to the new empy object
+// 3. calls constructor to 'build' the object
