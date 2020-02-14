2a4bef4f8698f5a83244bfcb04d7d7906c153453
Pierce <pierce.woods95@gmail.com>
2020-02-14 13:27:06 +0000

    [section_15] Class Methods & Method Chaining

--- Section_15[OOP]/sandbox.js
+++ Section_15[OOP]/sandbox.js
@@ -1,18 +1,26 @@
 class User {
     constructor(username, email) {
-        // set up properties
         this.username = username;
         this.email = email;
+        this.score = 0;
+    }
+    // don't comma seperate in classes
+    login() {
+        console.log(`${this.username} logged in`);
+        return this;
+    }
+    logout() {
+        console.log(`${this.username} logged out`);
+        return this;
+    }
+    incScore() {
+        this.score++;
+        console.log(`${this.username} has a score of: ${this.score}`);
+        return this;
     }
 };
 
 const userOne = new User('mario', 'mario@nintendo.com');
 const userTwo = new User('luigi', 'luigi@nintendo.com');
 
-console.log(userOne, userTwo);
-
-
-// the 'new' keyword
-// 1. creates empty object 
-// 2. binds value of 'this' to the new empy object
-// 3. calls constructor to 'build' the object
+userOne.login().incScore().logout();
