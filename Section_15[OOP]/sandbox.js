0dc03ef4bb19de3f4698e212f606fc69bf1be052
Pierce <pierce.woods95@gmail.com>
2020-02-14 14:52:25 +0000

    [section_15] Prototype Model

--- Section_15[OOP]/sandbox.js
+++ Section_15[OOP]/sandbox.js
@@ -2,45 +2,22 @@
 function User(username, email) {
     this.username = username;
     this.email = email;
-    this.login = function() {
-        console.log(`${username} has logged in`);
-    };
+    // this.login = function() {
+    //     console.log(`${username} has logged in`);
+    // };
 }
 
-// class User {
-//     constructor(username, email) {
-//         this.username = username;
-//         this.email = email;
-//         this.score = 0;
-//     }
-//     // don't comma seperate in classes
-//     login() {
-//         console.log(`${this.username} logged in`);
-//         return this;
-//     }
-//     logout() {
-//         console.log(`${this.username} logged out`);
-//         return this;
-//     }
-//     incScore() {
-//         this.score++;
-//         console.log(`${this.username} has a score of: ${this.score}`);
-//         return this;
-//     }
-// };
-
-// class Admin extends User {
-//     constructor(username, email, title) {
-//         super(username, email);
-//         this.title = title;
-//     }
-//     deleteUser(user) {
-//         users = users.filter(u => u.username !== user.username);
-//     }
-// };
+User.prototype.login = function() {
+    console.log(`${this.username} has logged in`);
+    return this;
+}
+User.prototype.logout = function() {
+    console.log(`${this.username} has logged out`);
+    return this;
+}
 
 const userOne = new User('mario', 'mario@nintendo.com');
 const userTwo = new User('luigi', 'luigi@nintendo.com');
 
 console.log(userOne, userTwo);
-userOne.login();
+userOne.login().logout();
